98e5194b44871c575a96286e0e4e3f6127fdfd09
Harlequelrah <degbovimax19@gmail.com>
2025-05-23 01:35:17 +0000

    debgage des warnings sur la duplication des operation_id

--- test_project/main.py
+++ test_project/main.py
@@ -6,7 +6,7 @@ from .settings.models_metadata import database
 from .settings.logger.router import logger_router
 from .settings.logger.model import Log
 from .settings.auth.configs import authentication_router
-from .settings.auth.routers import user_router
+from .settings.auth.routers import user_router, role_router,user_role_router,user_privilege_router,privilege_router,role_privilege_router
 app = FastAPI()
 
 
@@ -16,9 +16,13 @@ async def hello():
 
 app.include_router(authentication_router)
 app.include_router(user_router)
-app.include_router(logger_router)
+app.include_router(role_router)
+app.include_router(user_role_router)
+app.include_router(privilege_router)
+app.include_router(user_privilege_router)
+app.include_router(role_privilege_router)
 # app.include_router(app_myapp)
-
+app.include_router(logger_router)
 app.add_middleware(
     LoggerMiddleware, LogModel=Log, session_manager=database.session_manager
 )
